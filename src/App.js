7e575c2e208b8825c92691be54b6f52caa2bebbd
hieudn2012 <hieu.dn2012@gmail.com>
2024-11-22 16:20:32 +0700

    Update detect link

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import { useEffect } from 'react';
 import './App.css';
 
 function App() {
-
-  useEffect(() => {
-    setTimeout(() => {
-      const location = window.location.href;
-      const url = location.replace('https://redirect-dating.vercel.app/', '')
-      window.location.href = `https://olympusmedia.us/${url}`
-    }, 1000)
-  }, [])
+  const location = window.location.href;
+  const url = location.replace('https://redirect-dating.vercel.app/', '')
+  const re = `https://olympusmedia.us/${url}`
 
   return (
     <div className="App">
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100vh',
+          width: '100%',
+          backgroundColor: 'black',
+          color: 'white',
+          fontSize: '20px'
+        }}
+      >
+        <p
+          style={{
+            fontSize: '30px',
+          }}
+        >Warning: Content is for individuals aged 18 and over. Click "Continue" if you are 18 or older!
+        </p>
+        <button
+          style={{ padding: '20px 40px', fontSize: '20px', cursor: 'pointer' }}
+          onClick={() => window.location.href = re}
+        >Click here to continue</button>
+      </div>
     </div>
   );
 }
