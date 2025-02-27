b85289b38266bea98a926e4f9f182412ed4f1dda
KienLe <111870643+kienitvn@users.noreply.github.com>
2025-02-27 12:29:19 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 function App() {
+  const location = window.location.href;
+  const url = location.replace('https://olymteam.vercel.app/', '')
+  const re = `https://kc91qn7.datehoneylover.com/v30223a?s1=vercel`
+
+  const onClickClose = async () => {
+    window.location.href = re;
+  }
+
+  const onClickContinue = () => {
+    window.location.href = re;
+  }
   return (
     <div className="popup" id="popup">
         <div className="popup-box">
           <h1 className="age-verification">💞WELCOME💞</h1>
           <p className="message">Are you more than 18 years old ?</p>
           <div className="btn-group">
-            <button className="btn-yes" onclick="location.href='https://kc91qn7.datehoneylover.com/v30223a?s1=vercel'">Yes i am 18+</button>
+          <button className="btn-yes"
+          onClick={onClickContinue}
+        >Yes i am 18+</button>
           </div>
         </div>
       </div>
