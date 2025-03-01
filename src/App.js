4b8332f90a6c07af4dc6bbea1f30876226ea9dd2
KienLe <111870643+kienitvn@users.noreply.github.com>
2025-03-01 23:16:30 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react"
-import { useLocation } from "react-router"
+import { useLocation } from "react-router-dom"
 function App() {
   let local = useLocation();
   console.log(local);
