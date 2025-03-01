c16c447d9b18a63a58c7ed807b37d4d4b0cf8ca8
KienLe <111870643+kienitvn@users.noreply.github.com>
2025-03-01 16:38:24 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react"
-import { useLocation } from "react-router"
+import { useLocation } from "react-router-dom"
 function App() {
   let loca = useLocation();
   console.log(loca.pathname);
